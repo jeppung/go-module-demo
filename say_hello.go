2a81d3a68bac0eef222540ae83edb887d96fc12c
jeppung <jeppung@gmail.com>
2024-06-13 13:35:50 +0700

    feat: Adding sum number function

--- say_hello.go
+++ say_hello.go
@@ -5,3 +5,11 @@ import "fmt"
 func SayHello(text string) string {
 	return fmt.Sprintf("Hello, %s\n", text)
 }
+
+func SumNumber(numbers ...int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
